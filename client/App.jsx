3e53567459bb969529240715952def7be107efd1
Jackson Tong <jacksonktong@gmail.com>
2022-01-25 17:17:10 -0800

    2

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainContainer from './containers/MainContainer.jsx';
+
+const App = () => {
+  <div id="mainpage">
+    <MainContainer/>
+  </div>
+}
+
+export default App;
---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from '../components/searchbar.jsx';
+
+const MainContainer = props => {
+  <div className="container">
+    <h1 id="header">My Movie List</h1>
+    <SearchBar/>
+  </div>
+};
+
+export default MainContainer;
