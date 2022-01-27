e34db14c3095e1406c3bb7c853b45bf869f8e84e
Jackson Tong <jacksonktong@gmail.com>
2022-01-26 23:49:35 -0800

    onclick for favorites

--- client/containers/FavoriteMovies.jsx
+++ client/containers/FavoriteMovies.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const FavoriteMovies = props => {
+
+  // const grabFavMovies = () =>{
+  //   fetch('/api/favorites')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       return console.log(data)
+  //     });
+  // }
+
   return (
   <div className="favoriteMovies">
     <h1 id="head">Favorited Movies</h1>
