fefaa1d6ad12a8131edd8a8b80ae5a57ce23d179
Jackson Tong <jacksonktong@gmail.com>
2022-01-26 20:07:35 -0800

    restored changes and added fav mov comp

---
+++ client/containers/FavoriteMovies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoriteMovies = props => {
+  return (
+  <div className="favoriteMovies">
+    <h1 id="head">My Movie List</h1>
+  </div>
+  )
+};
+
+export default FavoriteMovies;
